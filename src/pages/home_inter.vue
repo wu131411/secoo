b126feab846286a3c6bee8bef85135187991ffb0
lizimeng <920295621@qq.com>
2017-11-14 16:13:59 +0800

    李子萌提交，页面完成，功能完善中

--- src/pages/home_inter.vue
+++ src/pages/home_inter.vue
@@ -78,7 +78,7 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
 .top_panel {
     width: 100%;
 }
--- src/pages/home_new.vue
+++ src/pages/home_new.vue
@@ -113,7 +113,7 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
  /*section*/
     .mtArea {
       margin-bottom: 0.26rem;
